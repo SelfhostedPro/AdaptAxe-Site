2941e334ad6019b3af8c32e8d7e6bffe5be7d9fb
SelfhostedPro <info@selfhosted.pro>
2025-02-27 16:11:39 -0800

    updating cloudflare

--- apps/home/open-next.config.ts
+++ apps/home/open-next.config.ts
@@ -1,28 +1,10 @@
-import incrementalCache from "@opennextjs/cloudflare/kv-cache";
-import type { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
+import kvIncrementalCache from "@opennextjs/cloudflare/kv-cache";
+import { defineCloudflareConfig } from "@opennextjs/cloudflare/config";
+import memoryQueue from "@opennextjs/cloudflare/memory-queue";
 
-const config: OpenNextConfig = {
-  default: {
-    override: {
-      wrapper: "cloudflare-node",
-      converter: "edge",
-      incrementalCache: async () => incrementalCache,
-      tagCache: "dummy",
-      queue: "dummy"
-    },
-  },
-  middleware: {
-    external: true,
-    override: {
-      wrapper: "cloudflare-edge",
-      converter: "edge",
-      proxyExternalRequest: "fetch",
-      generateDockerfile: true
-    },
-  },
-  dangerous: {
-    enableCacheInterception: true,
-  },
-};
+import { OpenNextConfig } from "@opennextjs/aws/types/open-next.js";
 
-export default config;
+export default defineCloudflareConfig({
+  incrementalCache: kvIncrementalCache,
+  queue: memoryQueue,
+}) as OpenNextConfig;
